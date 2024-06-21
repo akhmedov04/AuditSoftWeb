63d6d6b8b45e71860bb197023bc9cb37ed39f47c
akhmedov <bahodirdadajonov2004@gmail.com>
2024-06-21 08:42:06 -0500

    Blog page edited

--- AuditSoft/urls.py
+++ AuditSoft/urls.py
@@ -26,5 +26,6 @@ urlpatterns = [
     path('main/', main),
     path('profilecd /', profil),
     path('apteka/', include('apteka.urls')),
+    path('cafe/', include('resaud.urls')),
     path('user/', include('userapp.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- apteka/views.py
+++ apteka/views.py
@@ -1,18 +1,8 @@
 from django.shortcuts import render, redirect
 from .models import *
 
-# Create your views here.
 def home(request):
     data = {
         'blog': Maqola.objects.all()
     }
-    return render(request, 'apteka.html', data)
-
-# def main(request):
-#     if request.user.is_authenticated:
-#         data = {
-#             "user": request.user
-#         }
-#         return render(request, 'aptekalogged.html', data)
-#     return redirect('/apteka/')
-
+    return render(request, 'blog.html', data)
